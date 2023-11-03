ad18282358e6fc5f963ef6518b7f7c3179212360
13411 <13411193160a@gmail.com>
2023-11-02 20:33:25 -0800

    header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import './header.css';
+import { AiOutlineArrowRight } from 'react-icons/ai';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header'>
+      <div className='header-title'>
+        <p>Try our new meal</p>
+        <h1><span style={{color:"var(--primary-color)"}}>DOUBLE <br/></span><span style={{color:"var(--tertiary-color)"}}>NUKE<br/></span><span style={{color:"white"}}>BURGER</span></h1>
+        <button>Order Now!</button>
+        
+      </div>
+      <a href="" className='next' style={{color:"white"}}><AiOutlineArrowRight size={30} color='white'/></a>
+      <div className='bottom'>
+        <div id='active'></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
   )
 }
 
